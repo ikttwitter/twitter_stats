6281b006488dc8b5606c087f9e71dfb0de0e31d5
kalemdzievski <nikola.kalemdzievski@gmail.com>
2015-05-07 19:59:03 +0200

    Initial commit

---
+++ application/config/constants.php
@@ -0,0 +1,107 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| File and Directory Modes
+|--------------------------------------------------------------------------
+|
+| These prefs are used when checking and setting modes when working
+| with the file system.  The defaults are fine on servers with proper
+| security, but you may wish (or even need) to change the values in
+| certain environments (Apache running a separate process for each
+| user, PHP under CGI with Apache suEXEC, etc.).  Octal values should
+| always be used to set the mode correctly.
+|
+*/
+define('FILE_READ_MODE', 0644);
+define('FILE_WRITE_MODE', 0666);
+define('DIR_READ_MODE', 0755);
+define('DIR_WRITE_MODE', 0755);
+
+/*
+|--------------------------------------------------------------------------
+| File Stream Modes
+|--------------------------------------------------------------------------
+|
+| These modes are used when working with fopen()/popen()
+|
+*/
+
+define('FOPEN_READ', 'rb');
+define('FOPEN_READ_WRITE', 'r+b');
+define('FOPEN_WRITE_CREATE_DESTRUCTIVE', 'wb'); // truncates existing file data, use with care
+define('FOPEN_READ_WRITE_CREATE_DESTRUCTIVE', 'w+b'); // truncates existing file data, use with care
+define('FOPEN_WRITE_CREATE', 'ab');
+define('FOPEN_READ_WRITE_CREATE', 'a+b');
+define('FOPEN_WRITE_CREATE_STRICT', 'xb');
+define('FOPEN_READ_WRITE_CREATE_STRICT', 'x+b');
+
+/*
+|--------------------------------------------------------------------------
+| Display Debug backtrace
+|--------------------------------------------------------------------------
+|
+| If set to TRUE, a backtrace will be displayed along with php errors. If
+| error_reporting is disabled, the backtrace will not display, regardless
+| of this setting
+|
+*/
+define('SHOW_DEBUG_BACKTRACE', TRUE);
+
+/*
+|--------------------------------------------------------------------------
+| Exit Status Codes
+|--------------------------------------------------------------------------
+|
+| Used to indicate the conditions under which the script is exit()ing.
+| While there is no universal standard for error codes, there are some
+| broad conventions.  Three such conventions are mentioned below, for
+| those who wish to make use of them.  The CodeIgniter defaults were
+| chosen for the least overlap with these conventions, while still
+| leaving room for others to be defined in future versions and user
+| applications.
+|
+| The three main conventions used for determining exit status codes
+| are as follows:
+|
+|    Standard C/C++ Library (stdlibc):
+|       http://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
+|       (This link also contains other GNU-specific conventions)
+|    BSD sysexits.h:
+|       http://www.gsp.com/cgi-bin/man.cgi?section=3&topic=sysexits
+|    Bash scripting:
+|       http://tldp.org/LDP/abs/html/exitcodes.html
+|
+*/
+define('EXIT_SUCCESS', 0); // no errors
+define('EXIT_ERROR', 1); // generic error
+define('EXIT_CONFIG', 3); // configuration error
+define('EXIT_UNKNOWN_FILE', 4); // file not found
+define('EXIT_UNKNOWN_CLASS', 5); // unknown class
+define('EXIT_UNKNOWN_METHOD', 6); // unknown class member
+define('EXIT_USER_INPUT', 7); // invalid user input
+define('EXIT_DATABASE', 8); // database error
+define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
+define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+/*
+ |--------------------------------------------------------------------------
+ | Twitter API Keys
+ |--------------------------------------------------------------------------
+ |
+ */
+define('CONSUMER_KEY', 'b3EjCgg6NLjNkp8lkdshMnWSp'); // Consumer Key (API Key)
+define('CONSUMER_SECRET', 'f6PBQMMcAA04OwqUAU5sDCmbs7r7s77Lt5gFpE7EMtPZ5CwwwF'); // Consumer Secret (API Secret)
+define('ACCESS_TOKEN', '2351323935-uAwFjiVZir8plDOtAZrPdmzdbF6jrC2Lrn6od7B'); // Access Token
+define('ACCESS_SECRET', 'TFGpfQq0GI4nZOS3HF1Tki0So0KpcniBmdvCJvGQgYXWy'); // Access Token Secre
+
+define('CALL_BACK_URL', 'http://127.0.0.1/ikttwitter'); // Call back URL
+define('App-only authentication', 'https://api.twitter.com/oauth2/token'); // App-only authentication
+define('Request token URL', 'https://api.twitter.com/oauth/request_token'); // Request token URL
+define('Authorize URL', 'https://api.twitter.com/oauth/authorize'); // Authorize URL
+define('Access token URL', 'https://api.twitter.com/oauth/access_token'); // Access token URL
+
+
+
+
---
+++ application/views/api_test.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Twitter API Test</title>
+	</head>
+	
+	<body>
+		<div id="json"">
+			<?php echo ($json); ?>
+		</div>
+	</body>
+</html>
